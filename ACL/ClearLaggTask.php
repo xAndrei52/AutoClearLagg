48b6b952a658713d5385d6a7e540f99ecd4ba450
Potatoe <30378179+PotatoeTrainYT@users.noreply.github.com>
2018-01-27 10:18:23 +0000

    Create ClearLaggTask.php

---
+++ ACL/ClearLaggTask.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ACL;
+
+use pocketmine\scheduler\PluginTask;
+
+class ClearLaggTask extends PluginTask {
+
+	private $plugin, $player;
+
+	public function __construct(Main $main){
+		$this->main = $main;
+		parent::__construct($main);
+	}
+
+	public function onRun(int $currentTick) {
+          $settings = $this->main->settings;
+		if($settings->get("items") == true and $settings->get("mobs") == true) {
+              $this->main->clearItems();
+              $this->main->clearMobs();
+              $this->main->getServer()->broadcastMessage($settings->get("all-cleared-message"));
+          }
+		if($settings->get("items") == true and $settings->get("mobs") == false) {
+              $this->main->clearItems();
+              $this->main->getServer()->broadcastMessage($settings->get("items-cleared-message"));
+          }
+		if($settings->get("mobs") == true and $settings->get("items") == false) {
+              $this->main->clearMobs();
+              $this->main->getServer()->broadcastMessage($settings->get("mobs-cleared-message"));
+          }
+	}
+}
+
